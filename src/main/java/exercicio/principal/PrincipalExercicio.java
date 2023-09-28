fb2654a375787ef0c11a79d39e7e52ab86ee8c29
Wandberg de Lima <wandberg@gmail.com>
2023-09-28 14:16:06 -0300

    - A aplicar seus conhecimentos em uma aplicação de um sistema de músicas, utilizando conceitos de orientação a objetos, encapsulamento, herança e polimorfismo.

---
+++ src/main/java/exercicio/principal/PrincipalExercicio.java
@@ -0,0 +1,37 @@
+package exercicio.principal;
+
+import exercicio.modelos.MinhasPreferidas;
+import exercicio.modelos.Musica;
+import exercicio.modelos.Podcast;
+
+public class PrincipalExercicio {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Forever");
+        minhaMusica.setCantor("Kiss");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("BolhaDev");
+        meuPodcast.setApresentador("Marcus Mendes");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+
+    }
+}
