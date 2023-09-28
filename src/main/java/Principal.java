969f5103d9c80b6f6e8406849d30ef269f6f278a
Wandberg de Lima <wandberg@gmail.com>
2023-09-28 09:45:51 -0300

    - O problema de ter códigos duplicados em várias classes, utilizando a herança como solução; - A palavra-chave extends, utilizada para indicar que uma classe herda de outra; - Como sobrescrever um método sendo herdado, com o uso opcional da anotação @Override; - Como aplicar o conceito de polimorfismo no código, evitando métodos duplicados.

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,15 +1,15 @@
+import calculos.CalculadoraDeTempo;
 import modelo.Filme;
+import modelo.Serie;
 
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-
         meuFilme.setNome("O poderoso chefão");
         meuFilme.setAnoDeLancamento(1970);
         meuFilme.setDuracaoEmMinutos(180);
 
         meuFilme.exibeFichaTecnica();
-
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
@@ -17,6 +17,26 @@ public class Principal {
         System.out.println("Total de Avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
 
+        Serie lost = new Serie();
 
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+
+        System.out.println("Duração para Maratonar "+ lost.getNome() + ": " + lost.getDuracaoEmMinutos());
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.incluir(meuFilme);
+        calculadora.incluir(outroFilme);
+        calculadora.incluir(lost);
+        System.out.println("Tempo total para Maratonar tudo: " + calculadora.getTempoTotal());
     }
 }
